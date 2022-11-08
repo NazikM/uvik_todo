2548a11036042150aaffbc008b9133f93e9b9ca6
Nazar Marusyn <marusinnazar@gmail.com>
2022-11-08 10:58:58 +0200

    Update db.py to async format.

--- db.py
+++ db.py
@@ -1,102 +1,101 @@
-import contextlib
-import sqlite3
+from databases import Database
 
 
-def create_tables():
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('''CREATE TABLE IF NOT EXISTS user(
-            telegram_id INTEGER NOT NULL, 
-            state text NOT NULL,
-            draft_task_id INTEGER
-            );''')
-            # data = cur.fetchone()
-
-        with con as cur:
-            cur.execute('''CREATE TABLE IF NOT EXISTS task(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL, 
-            description TEXT, 
-            image BLOB, 
-            completed INTEGER, 
-            telegram_id INTEGER NOT NULL)''')
+async def add_user(telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        await database.execute(
+            "INSERT INTO user(telegram_id, state, draft_task_id) VALUES(:telegram_id, 'main', Null);",
+            {'telegram_id': telegram_id})
 
 
-def add_user(telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('INSERT INTO user(telegram_id, state, draft_task_id) VALUES(?, ?, Null);', (telegram_id, 'main'))
+async def add_task(title, telegram_id, description=None, image_blob=None):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        async with database.connection() as connection:
+            async with connection.transaction():
+                await connection.execute(
+                    'INSERT INTO task(title, description, image, completed, telegram_id) VALUES(:title, :desc, :img, false, :teleg_id);',
+                    {'title': title,
+                     'desc': description,
+                     'img': image_blob,
+                     'teleg_id': telegram_id})
+                return await connection.fetch_one(query="SELECT last_insert_rowid()")
 
 
-def add_task(title, telegram_id, description=None, image_blob=None):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with contextlib.closing(con.cursor()) as cur:
-            cur.execute('INSERT INTO task(title, description, image, completed, telegram_id) VALUES(?, ?, ?, false, ?);', (title, description, image_blob, telegram_id))
-            con.commit()
-            return cur.lastrowid
+async def get_draft_task_id(telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        query = 'select draft_task_id FROM user where telegram_id=:teleg_id'
+        params = {
+            'teleg_id': telegram_id
+        }
+        return await database.fetch_one(query=query, values=params)
 
 
-def get_draft_task_id(telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with contextlib.closing(con.cursor()) as cur:
-            cur.execute('select draft_task_id FROM user where telegram_id=?', (telegram_id,))
-            return cur.fetchone()[0]
+async def set_draft_task_id(telegram_id, task_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        await database.execute('UPDATE user SET draft_task_id=:task_id WHERE telegram_id=:teleg_id;',
+                               {'task_id': task_id, 'teleg_id': telegram_id})
 
 
-def set_draft_task_id(telegram_id, task_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('UPDATE user SET draft_task_id=? WHERE telegram_id=?;', (task_id, telegram_id))
+async def mark_as_completed(task_id, telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        await database.execute('UPDATE task SET completed=true WHERE id=:task_id and telegram_id=:teleg_id;',
+                               {'task_id': task_id, 'teleg_id': telegram_id})
 
 
-def mark_as_completed(task_id, telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('UPDATE task SET completed=true WHERE id=(?) and telegram_id=?;', (task_id, telegram_id))
+async def update_task(task_id, telegram_id, title=None, description=None, image_blob=None):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        if title:
+            await database.execute('UPDATE task SET title=:title WHERE id=:task_id and telegram_id=:teleg_id;',
+                                   {'title': title,
+                                    'task_id': task_id,
+                                    'teleg_id': telegram_id})
+        if description:
+            await database.execute('UPDATE task SET description=:desc WHERE id=:task_id and telegram_id=:teleg_id;',
+                                   {'desc': description,
+                                    'task_id': task_id,
+                                    'teleg_id': telegram_id})
+        if image_blob:
+            await database.execute('UPDATE task SET image=:img WHERE id=:task_id and telegram_id=:teleg_id;',
+                                   {'img': image_blob,
+                                    'task_id': task_id,
+                                    'teleg_id': telegram_id})
 
 
-def update_task(task_id, telegram_id, title=None, description=None, image_blob=None):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            if title:
-                cur.execute('UPDATE task SET title=(?) WHERE id=(?) and telegram_id=(?);', (title, task_id, telegram_id))
-            if description:
-                cur.execute('UPDATE task SET description=(?) WHERE id=(?) and telegram_id=(?);', (description, task_id, telegram_id))
-            if image_blob:
-                cur.execute('UPDATE task SET image=(?) WHERE id=(?) and telegram_id=(?);', (image_blob, task_id, telegram_id))
+async def list_user_tasks(telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        query = 'select * FROM task where telegram_id=:teleg_id'
+        params = {
+            'teleg_id': telegram_id
+        }
+        return await database.fetch_all(query=query, values=params)
 
 
-def list_user_tasks(telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with contextlib.closing(con.cursor()) as cur:
-            cur.execute('select * FROM task where telegram_id=?', (telegram_id,))
-            data = cur.fetchall()
-            return data
+async def get_task(task_id, telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        query = 'select * FROM task where telegram_id=:teleg_id and id=:task_id'
+        params = {
+            'teleg_id': telegram_id,
+            'task_id': task_id
+        }
+        return await database.fetch_one(query=query, values=params)
 
 
-def get_task(task_id, telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with contextlib.closing(con.cursor()) as cur:
-            cur.execute('select * FROM task where telegram_id=? and id=?', (telegram_id, task_id))
-            data = cur.fetchall()
-            return data[0]
+async def delete_user_task(task_id, telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        await database.execute('DELETE FROM task WHERE id=:task_id and telegram_id=:teleg_id',
+                               {'task_id': task_id, 'teleg_id': telegram_id})
 
 
-def delete_user_task(task_id, telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('DELETE FROM task WHERE id=? and telegram_id=?', (task_id, telegram_id))
+async def get_user_state(telegram_id):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        query = 'select state FROM user where telegram_id=:teleg_id'
+        params = {
+            'teleg_id': telegram_id
+        }
+        return await database.fetch_one(query=query, values=params)
 
 
-def get_user_state(telegram_id):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with contextlib.closing(con.cursor()) as cur:
-            cur.execute('select state FROM user where telegram_id=?', (telegram_id,))
-            state = cur.fetchone()[0]
-    return state
-
-
-def set_user_state(telegram_id, state):
-    with contextlib.closing(sqlite3.connect('todo.db')) as con:
-        with con as cur:
-            cur.execute('UPDATE user SET state=(?) WHERE telegram_id=(?);', (state, telegram_id))
+async def set_user_state(telegram_id, state):
+    async with Database('sqlite+aiosqlite:///todo.db') as database:
+        await database.execute('UPDATE user SET state=:state WHERE telegram_id=:teleg_id;',
+                               {'state': state, 'teleg_id': telegram_id})
---
+++ db_crereation.py
@@ -0,0 +1,22 @@
+import contextlib
+import sqlite3
+
+
+def create_tables():
+    with contextlib.closing(sqlite3.connect('todo.db')) as con:
+        with con as cur:
+            cur.execute('''CREATE TABLE IF NOT EXISTS user(
+            telegram_id INTEGER NOT NULL, 
+            state text NOT NULL,
+            draft_task_id INTEGER
+            );''')
+            # data = cur.fetchone()
+
+        with con as cur:
+            cur.execute('''CREATE TABLE IF NOT EXISTS task(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL, 
+            description TEXT, 
+            image BLOB, 
+            completed INTEGER, 
+            telegram_id INTEGER NOT NULL)''')
