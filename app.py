192bd1a4b2650601adf83d20e779360c3db5752d
Nazar Marusyn <marusinnazar@gmail.com>
2022-11-10 21:51:51 +0200

    Update routes.

--- app.py
+++ app.py
@@ -65,58 +65,79 @@ async def process_remove_task_command(message: types.Message):
     await bot.send_message(telegram_id, res)
 
 
+async def add_task_title(msg, telegram_id):
+    task_id = await db.add_task(title=msg.text, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'add_task_1')
+    await db.set_draft_task_id(telegram_id, tuple(task_id)[0])
+    await bot.send_message(telegram_id, "Enter description or send 'skip' word to skip this step.")
+
+
+async def add_task_description(msg, telegram_id):
+    if msg.text != 'skip':
+        task_id = (await db.get_draft_task_id(telegram_id=telegram_id))[0]
+        await db.update_task(description=msg.text, task_id=task_id, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'add_task_2')
+    await bot.send_message(telegram_id, "Send me an help image or send 'skip' word to skip this step.")
+
+
+async def add_task_img(msg, telegram_id):
+    if msg.text != 'skip':
+        task_id = (await db.get_draft_task_id(telegram_id=telegram_id))[0]
+        await msg.photo[-1].download(destination_file=f'images/{task_id}')
+        with open(f'images/{task_id}', 'rb') as f:
+            content = f.read()
+        os.remove(f"images/{task_id}")
+        await db.update_task(image_blob=content, task_id=task_id, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'main')
+    await bot.send_message(telegram_id, "Successfully added task!")
+
+
+async def remove_task(msg, telegram_id):
+    task_id = msg.text
+    await db.delete_user_task(task_id=task_id, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'main')
+    await bot.send_message(telegram_id, "Task successfully removed")
+
+
+async def mark_task(msg, telegram_id):
+    task_id = msg.text
+    await db.mark_as_completed(task_id=task_id, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'main')
+    await bot.send_message(telegram_id, "Task successfully marked as completed ✅")
+
+
+async def detail_task(msg, telegram_id):
+    task_id = msg.text
+    task = await db.get_task(task_id=task_id, telegram_id=telegram_id)
+    await db.set_user_state(telegram_id, 'main')
+    res = f"ID: {task[0]}\nTitle: {task[1]}\nDescription: {task[2]}\nCompleted: "
+    res += ("✅" if task[4] else "❌")
+    await bot.send_message(telegram_id, res)
+    if task[3]:
+        await bot.send_photo(telegram_id, task[3])
+
+
+async def main_state(msg, telegram_id):
+    await bot.send_message(telegram_id, f"{msg.text} is not a command!")
+
+
 @dp.message_handler(content_types=['text', 'photo'])
 async def echo_message(message: types.Message):
     telegram_id = message.from_user.id
     user_state = (await db.get_user_state(telegram_id))[0]
-    if user_state == 'main':
-        await message.reply("Please enter command.")
-    elif user_state == 'add_task_0':
-        task_id = await db.add_task(title=message.text, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'add_task_1')
-        await db.set_draft_task_id(telegram_id, tuple(task_id)[0])
-        await bot.send_message(telegram_id, "Enter description or send 'skip' word to skip this step.")
-    elif user_state == 'add_task_1':
-        if message.text != 'skip':
-            task_id = (await db.get_draft_task_id(telegram_id=telegram_id))[0]
-            await db.update_task(description=message.text, task_id=task_id, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'add_task_2')
-        await bot.send_message(telegram_id, "Send me an help image or send 'skip' word to skip this step.")
-    elif user_state == 'add_task_2':
-        if message.text != 'skip':
-            task_id = (await db.get_draft_task_id(telegram_id=telegram_id))[0]
-            await message.photo[-1].download(destination_file=f'images/{task_id}')
-            with open(f'images/{task_id}', 'rb') as f:
-                content = f.read()
-            os.remove(f"images/{task_id}")
-            await db.update_task(image_blob=content, task_id=task_id, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'main')
-        await bot.send_message(telegram_id, "Successfully added task!")
 
-    elif user_state == 'remove_task_0':
-        task_id = message.text
-        await db.delete_user_task(task_id=task_id, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'main')
-        await bot.send_message(telegram_id, "Task successfully removed")
-
-    elif user_state == 'mark_task_0':
-        task_id = message.text
-        await db.mark_as_completed(task_id=task_id, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'main')
-        await bot.send_message(telegram_id, "Task successfully marked as completed ✅")
-
-    elif user_state == 'detail_task_0':
-        task_id = message.text
-        task = await db.get_task(task_id=task_id, telegram_id=telegram_id)
-        await db.set_user_state(telegram_id, 'main')
-        res = f"ID: {task[0]}\nTitle: {task[1]}\nDescription: {task[2]}\nCompleted: "
-        res += ("✅" if task[4] else "❌")
-        await bot.send_message(telegram_id, res)
-        if task[3]:
-            await bot.send_photo(telegram_id, task[3])
-    # await bot.send_message(message.from_user.id, message.text)
+    routes = {
+        "main": main_state,
+        "add_task_0": add_task_title,
+        "add_task_1": add_task_description,
+        "add_task_2": add_task_img,
+        "remove_task_0": remove_task,
+        "mark_task_0": mark_task,
+        "detail_task_0": detail_task
+    }
+    await routes[user_state](message, telegram_id)
 
 
 if __name__ == '__main__':
-    db.create_tables()
+    # db.create_tables()
     executor.start_polling(dp)
